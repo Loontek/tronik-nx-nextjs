4f87095178c572e3628192a08729d2ee0e73fbeb
Mark <104220213+Loontek@users.noreply.github.com>
2024-10-04 15:28:23 +0300

    update app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ToasterProvider } from '@/providers/toaster-provider';
 import Header from '@/components/header';
+import {redirect} from "next/navigation";
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
